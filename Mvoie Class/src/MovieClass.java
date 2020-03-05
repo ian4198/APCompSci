28aefd82f402998a40637293f555c0d8e3bcecfa
ian4198 <61471196+ian4198@users.noreply.github.com>
2020-03-05 13:17:55 -0500

    changes

---
+++ Mvoie Class/src/MovieClass.java
@@ -0,0 +1,78 @@
+
+public class MovieClass {
+	
+	String title;
+	String Studio;
+	String Rating;
+	int year;
+	public MovieClass()
+	{
+		String title = "Pending";
+		String Studio = "Pending";
+		String Rating = "Pending";
+		int year = 0000;
+	}
+	
+	public void createMovie(String a, String b, String c, int d)
+	{
+		String title = a;
+		String Studio = b;
+		String Rating = c;
+		int year = d;
+	}
+	
+	public void setTitle(String a)
+	{
+		String title = a;
+	}
+	public void setStudio(String b)
+	{
+		String Studio = b;
+	}
+	public void setRating(String c)
+	{
+		String Rating = c;
+	}
+	public void setYear(int d)
+	{
+		int year = d;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	public String getStudio()
+	{
+		return Studio;
+	}
+	public String getRating()
+	{
+		return Rating;
+	}
+	public int getYear()
+	{
+		return year;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
